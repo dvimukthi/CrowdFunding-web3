36f9563c59b03146fb2466fc848f448dbc4982f6
dvimukthi <deshanvimukthi59@gmail.com>
2022-12-15 21:59:51 +0530

    struct Campaign

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadlin;
+        uint256 amountColected;
+        string image;
+        address[] donators;
+        address[] donations;
+    }
 }
