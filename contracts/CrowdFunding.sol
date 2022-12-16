2869063cb5d9ea38c087b72f739741844dc97283
dvimukthi <deshanvimukthi59@gmail.com>
2022-12-16 20:36:31 +0530

    campaign.donators

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -50,6 +50,9 @@ contract CrowdFunding {
         uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
     }
 
     function getDonators() {}
